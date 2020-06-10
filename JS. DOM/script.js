cdca7d8227324e59085790c989d07b7090bddc8f
tarikk98 <61149797+tarikk98@users.noreply.github.com>
2020-06-10 21:16:51 +0300

    Add files via upload

---
+++ JS. DOM/script.js
@@ -0,0 +1,7 @@
+let div = document.createElement('div');
+  div.innerHTML='<b>Тарабанов Михаил Александрович</b>';
+  document.body.prepend(div);
+
+let elem = document.createElement('p');
+  elem.innerHTML='<b>21.10.1998</b>';
+  some_div.append(elem);
